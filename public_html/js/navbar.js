afd58eedb94a92c8d0ae6f022191798cdf537529
Ana Lemos <ana.lemos739@gmail.com>
2019-12-18 10:40:23 -0300

    organizando

--- public_html/js/navbar.js
+++
@@ -1,9 +0,0 @@
-jQuery(function () {
-    jQuery(window).scroll(function () {
-      if (jQuery(this).scrollTop() > 480) {
-       $("#menu").addClass("menu_scroll");
-      } else {
-       $("#menu").removeClass("menu_scroll");
-      }
-    });
-  });
